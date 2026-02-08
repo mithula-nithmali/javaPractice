1057b50e82df08b716a5b2a4333a28da5d3d0dbb
mithula <mithula@dmsswt.com>
2026-02-08 16:13:42 +0530

    Check if a Given Integer is Odd or Even

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int c=4;
-        int b=7;
-        int a;
-        // a= 4
-        a = c;
-        //c = 7
-        c = b;
-        b= 4;
-        b= a;
-        System.out.print("c"+c);
-        System.out.print("b"+b);
+     int input = 6;
+
+     if(input%2 == 1){
+        System.out.print("This is an odd number");
+     }else{
+         System.out.print("This is an even number");
+     }
     }
 }
